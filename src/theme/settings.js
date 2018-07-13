fcd94ccb1259cf825b109660d2402f0e653f5717
David Luhr <davidc.luhr@gmail.com>
2018-07-12 22:13:21 -0600

    Add colorbook and theme variables

---
+++ src/theme/settings.js
@@ -0,0 +1,18 @@
+// Colorbook
+export const charcoal = '#1f1f1f';
+export const linen = '#f2efe9';
+export const peacock = '#2c4747';
+export const plum = '#472c2c';
+
+// Typography
+export const sans = `'Open Sans', sans-serif`;
+export const serif = `'Old Standard TT', serif`;
+export const weightLight = 300;
+export const weightRegular = 400;
+export const weightSemiBold = 600;
+export const weightBold = 700;
+export const extraBold = 800;
+
+// Design elements
+export const borderRadius = '0.2rem';
+export const transition = '300ms ease-in-out';
