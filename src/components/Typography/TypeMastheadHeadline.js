dfb5e68202e80f17877837c6c3c43888c997c466
David Luhr <david.luhr@dockyard.com>
2018-08-27 08:52:28 -0600

    34 Add masthead type spec

---
+++ src/components/Typography/TypeMastheadHeadline.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import {
+  colorCharcoal,
+  fontFamilySerif,
+  weightRegular
+} from '../../theme/settings';
+
+const TypeMastheadHeadline = styled.span`
+  font-family: ${fontFamilySerif};
+  font-size: 9.6rem;
+  font-weight: ${weightRegular};
+  color: ${colorCharcoal};
+  line-height: 1.167;
+`;
+
+export default TypeMastheadHeadline;
