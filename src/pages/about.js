4d2946ea8dc4b2486d33775da3998aad0d052be4
David Luhr <david.luhr@dockyard.com>
2018-10-12 17:39:56 +0200

    Add heroine image grid

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 
 import GlobalLayout from '../components/Layout/GlobalLayout';
 import Main from '../components/Layout/Main';
+import ScreenReaderOnly from '../components/A11y/ScreenReaderOnly';
 import ContactForm from '../components/Content/ContactForm';
+import HeroineGrid from '../components/UI/Heroines/HeroineGrid';
 
 const AboutPage = () => (
   <StaticQuery
@@ -19,12 +22,21 @@ const AboutPage = () => (
     render={data => (
       <GlobalLayout>
         <Main>
-          <h1>About {data.site.siteMetadata.title}</h1>
+          <StyledHiddenTitle>About {data.site.siteMetadata.title}</StyledHiddenTitle>
+          <HeroineGrid
+            headline="Oh, Hello There"
+            imageStartSrc="https://source.unsplash.com/random/1410x1058"
+            imageStartAlt="Jacque and Sara sitting together"
+            imageEndSrc="https://source.unsplash.com/random/840x1120"
+            imageEndAlt="Jacque and Sara walking down stairs"
+          />
           <ContactForm />
         </Main>
       </GlobalLayout>
     )}
   />
 );
+
+const StyledHiddenTitle = ScreenReaderOnly.withComponent('h1');
 
 export default AboutPage;
